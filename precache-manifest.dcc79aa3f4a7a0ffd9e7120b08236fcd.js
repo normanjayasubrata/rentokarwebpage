334d79a84d8a59037b0229dbda11664c7fdb0478
normanjayasubrata <norman.jayasubrata@gmail.com>
2018-12-01 05:15:37 +0700

    Updates

---
+++ precache-manifest.dcc79aa3f4a7a0ffd9e7120b08236fcd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "c07bd8b0df81e6968d06be7664988714",
+    "url": "/rentokarwebpage/static/media/1.c07bd8b0.png"
+  },
+  {
+    "revision": "7a2c88c38d1006ac1ee4",
+    "url": "/rentokarwebpage/static/css/main.5d980a3b.chunk.css"
+  },
+  {
+    "revision": "67fa5fccd0aa44256654f9d48d40e7be",
+    "url": "/rentokarwebpage/static/media/WESTM.67fa5fcc.woff"
+  },
+  {
+    "revision": "652de5dfc7da3051db7f",
+    "url": "/rentokarwebpage/static/js/1.652de5df.chunk.js"
+  },
+  {
+    "revision": "1d9541376726123143ec",
+    "url": "/rentokarwebpage/static/js/runtime~main.1d954137.js"
+  },
+  {
+    "revision": "41ad2a4a70c156b8cfea5cd68b8e5352",
+    "url": "/rentokarwebpage/static/media/rentokar_logo_min.41ad2a4a.jpeg"
+  },
+  {
+    "revision": "df7beebcbc42f806b83c577012c226fd",
+    "url": "/rentokarwebpage/static/media/teaser_rentokar.df7beebc.mp4"
+  },
+  {
+    "revision": "a7d8fbabddde2a21e5e9b187385ca363",
+    "url": "/rentokarwebpage/static/media/rentokar_logo_max_white.a7d8fbab.png"
+  },
+  {
+    "revision": "7a2c88c38d1006ac1ee4",
+    "url": "/rentokarwebpage/static/js/main.7a2c88c3.chunk.js"
+  },
+  {
+    "revision": "3b87138b43fb8a88efd5f49c4938da4c",
+    "url": "/rentokarwebpage/static/media/2.3b87138b.png"
+  },
+  {
+    "revision": "9d1fa38ab27307442f46114354a6bc9c",
+    "url": "/rentokarwebpage/static/media/3.9d1fa38a.png"
+  },
+  {
+    "revision": "23488ea418c65221be37685c5c2da67a",
+    "url": "/rentokarwebpage/static/media/Caribbean_Island.23488ea4.woff"
+  },
+  {
+    "revision": "3d3b19e05aadde4607b694752db4721e",
+    "url": "/rentokarwebpage/static/media/Walk-Around-the-Block.3d3b19e0.woff"
+  },
+  {
+    "revision": "91696e0984cfab74ff344325e5015da5",
+    "url": "/rentokarwebpage/static/media/Montserrat-Regular.91696e09.otf"
+  },
+  {
+    "revision": "652de5dfc7da3051db7f",
+    "url": "/rentokarwebpage/static/css/1.5f0c14ac.chunk.css"
+  },
+  {
+    "revision": "f65dc5ce07c8265c6ede362e60cc62db",
+    "url": "/rentokarwebpage/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/rentokarwebpage/precache-manifest.dcc79aa3f4a7a0ffd9e7120b08236fcd.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/rentokarwebpage/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});

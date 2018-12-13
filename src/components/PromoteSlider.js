1207477bf7bb0452bd753649c4199ef112d24013
normanjayasubrata <norman.jayasubrata@gmail.com>
2018-12-13 08:39:54 +0700

    legend modification

--- src/components/PromoteSlider.js
+++ src/components/PromoteSlider.js
@@ -18,15 +18,15 @@ class PromoteSlider extends Component {
         >
           <div>
             <img src={image1} alt="image1" />
-            <p className="legend">Legend 1</p>
+            <p className="legend">Temukan barang uang ingin kamu sewa</p>
           </div>
           <div>
             <img src={image2} alt="image2" />
-            <p className="legend">Legend 2</p>
+            <p className="legend">Sewa barang yang kamu inginkan</p>
           </div>
           <div>
             <img src={image3} alt="image3" />
-            <p className="legend">Legend 3</p>
+            <p className="legend">Kembalikan barang yang kamu sewa tepat waktu</p>
           </div>
         </Carousel>
       </div>
